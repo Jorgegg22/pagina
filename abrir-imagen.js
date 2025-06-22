3cbef1c7a645dbae1d4c2cef2a4c848de26bec80
Jorge <jgg2232004@gmail.com>
2025-06-22 21:49:21 +0200

    Pagina producto idea

--- abrir-imagen.js
+++ abrir-imagen.js
@@ -1,9 +1,30 @@
 const modal = document.getElementById("modalImagen");
 const imgProducto = document.getElementById("imgProducto");
+const imgProducto1 = document.getElementById("imgProducto1");
+const imgProducto2 = document.getElementById("imgProducto2");
+const imgProducto3 = document.getElementById("imgProducto3");
 const imgGrande = document.getElementById("imgGrande");
 const cerrarModal = document.getElementById("cerrarModal");
 
 imgProducto.onclick = function() {
+  modal.style.display = "block";
+  imgGrande.src = this.src;
+  imgGrande.alt = this.alt;
+}
+
+imgProducto1.onclick = function() {
+  modal.style.display = "block";
+  imgGrande.src = this.src;
+  imgGrande.alt = this.alt;
+}
+
+imgProducto2.onclick = function() {
+  modal.style.display = "block";
+  imgGrande.src = this.src;
+  imgGrande.alt = this.alt;
+}
+
+imgProducto3.onclick = function() {
   modal.style.display = "block";
   imgGrande.src = this.src;
   imgGrande.alt = this.alt;
